d340b0834efed6d0b449739f41759f116c4367b3
afrulsandi1001 <afrulsandi69@gmail.com>
2021-03-15 12:35:31 +0700

    menambahkan latihan 1 dan latihan 2 pada minggu kedua

---
+++ minggu2/latihan1/loop-for.php
@@ -0,0 +1,23 @@
+<?php
+$bil = 5;
+for($i=1;$i<=$bil;$i++){
+ for($j=1;$j<=$i;$j++){
+ echo $i;
+ }
+ echo '<br/>';
+}
+?>
+
+<?php
+	$bil=5;
+	$i=0;
+		while($i<$bil){
+ 	$i++;
+ 	$j=0;
+ 		while($j<$i){
+ 	$j++;
+ echo $i;
+}
+ echo '<br/>';
+}
+?>
