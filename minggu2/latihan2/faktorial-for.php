808917d26b33e248cdd3a859d73184a22a703485
afrulsandi1001 <afrulsandi69@gmail.com>
2021-03-15 12:47:29 +0700

    merubah sebgaian isi file

--- minggu2/latihan2/faktorial-for.php
+++ minggu2/latihan2/faktorial-for.php
@@ -1,7 +1,11 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Faktorial</title>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Faktorial</title>
 </head>
 <body>
 <form name="frm01" method="post">
