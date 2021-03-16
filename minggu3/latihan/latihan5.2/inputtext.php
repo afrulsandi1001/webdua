a304542cd0f5c429955b0af41fb782599a2e5c2b
afrulsandi1001 <afrulsandi69@gmail.com>
2021-03-16 15:32:28 +0700

    menambahkan folder minggu3

---
+++ minggu3/latihan/latihan5.2/inputtext.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengolahan Form ~ Text</title>
+</head>
+
+<body>
+    <form action="prosestext.php" method="post" name="input">
+        Nama Anggota <br>
+        <input type="text" name="nama1"><br>
+        <input type="text" name="nama2"><br>
+        <input type="text" name="nama3"><br>
+        <input type="text" name="nama4"><br>
+        <input type="submit" name="Input" value="Input">
+    </form>
+</body>
+
+</html>
